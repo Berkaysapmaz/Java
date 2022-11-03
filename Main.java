598dd3d860b21d10ce109a2825f271653efe199b
Berkaysapmaz <berkay30082000@hotmail.com>
2022-11-04 02:36:22 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,6 +24,13 @@ class Main {
         String soyisim = "Sapmaz";
         String fullname = isim +" "+ soyisim;
         System.out.println(fullname);
-
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
     }
 }
