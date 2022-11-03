3cc09936d70350cb153058731e26b33a22813a94
Berkaysapmaz <berkay30082000@hotmail.com>
2022-11-04 02:29:56 +0300

    learning basics java

--- Main.java
+++ Main.java
@@ -1,8 +1,29 @@
+import javax.net.ssl.SSLContext;
 
 // Your First Program
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("First Java Code"); 
+        System.out.print("We will");
+        System.out.println(" Rock you");
+        System.out.println(5+5);
+        System.out.println(5*5);
+        System.out.println(5/5);
+        //This is a comment
+        /*Multi Line 
+        comment */ 
+        String name = "Berkay";
+        System.out.println(name);
+        int myNum = 15;
+        System.out.println(myNum);
+        myNum = 20;
+        System.out.println("myNum su anda"+" " + myNum);
+        String isim = "Berkay";
+        String soyisim = "Sapmaz";
+        String fullname = isim +" "+ soyisim;
+        System.out.println(fullname);
+
     }
 }
